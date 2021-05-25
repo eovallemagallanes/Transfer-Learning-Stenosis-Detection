1fa67b05d16a20dc5ebe0af7273db5fa0480a52c
eovallemagallanes <e.ovallemagallanes@ugto.mx>
2021-05-25 16:21:18 -0500

    Update main_test.py

--- main_test.py
+++ main_test.py
@@ -1,16 +1,12 @@
 import torch
-from torchvision import datasets, transforms
 
-import os
 import sys
 import configparser
 
-
-from models.ResNet import ResidualNet
 from test import test_model, eval_preds
+from models.utilsNet import createModel, createTestDataLoaders
+from gradcam.GradCam import GradCam, computeGradCam
 
-# set manual seed
-torch.manual_seed(42)
 
 # Detect if we have a GPU available
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -24,51 +20,61 @@ print('Argument List:', str(sys.argv))
 config = configparser.ConfigParser()
 config.read(sys.argv[1])
 
+# weights dir
 DATA_DIR = config.get('PARAMS', 'DATA_DIR')
 WEIGHTS_DIR = config.get('PARAMS', 'WEIGHTS_DIR')
+
+# model configuration
+model_type = config.get('PARAMS', 'model_type')
+model_deep = int(config.get('PARAMS', 'model_deep'))
 model_name = config.get('PARAMS', 'model_name')
 cut_block = int(config.get('PARAMS', 'cut_block'))
 train_layers = int(config.get('PARAMS', 'train_layers'))
+imagenet_norm = True if config.get('PARAMS', 'imagenet_norm') == 'True' else False
+cam_layer = config.get('PARAMS', 'cam_layer')
+
+# results dir
+RESULTS_DIR = config.get('PARAMS', 'RESULTS_DIR')
+GRADCAM = True if config.get('PARAMS', 'GRADCAM') == 'True' else False
 
 print('*'*50)
 print('Model: {}'.format(model_name))
 print('Cut block: {}'.format(cut_block))
 print('Num train_layers: {}'.format(train_layers))
 
-# define data transformations
-data_transforms = transforms.Compose(
-        [
-            transforms.Grayscale(num_output_channels=3),
-            transforms.ToTensor(),
-            # Normalize input channels using mean values and standard deviations of ImageNet.
-            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
-        ]
-    )
-
-
 # create dataloaders
-test_image_datasets = datasets.ImageFolder(os.path.join(DATA_DIR, "test"), data_transforms)
-test_loader = torch.utils.data.DataLoader(dataset=test_image_datasets, shuffle=False, batch_size=1)
-
+# if imagenet_norm, apply ImageNet normalization
+test_loader = createTestDataLoaders(DATA_DIR, batch_size=1, num_output_channels=3, normalize=imagenet_norm)
 
 # create model
-model = ResidualNet(model_name= model_name, num_blocks=cut_block, num_classes=1, num_trainable_layers=-1, pretrained=False)
+model = createModel(model_type=model_type, model_deep=model_deep, num_blocks=cut_block, num_trainable_layers=train_layers,
+                    pretrained=False, num_classes=1)
 model.to(device)
 
 # load weights
-model_weights = '%s/state_dict_model_%s_%02d_%02d.pth' %(WEIGHTS_DIR, model_name, cut_block, train_layers)
-pretrained_checkpoint = torch.load(model_weights)
+model_weights = '%s/state_dict_model_%s.pth' %(WEIGHTS_DIR, model_name)
+pretrained_checkpoint = torch.load(model_weights,  map_location=device)
 model.load_state_dict(pretrained_checkpoint['model_state_dict'])
-model.eval()
+#model.eval()
 
 # test
-results_report = '%s/model_results_%s_%02d_%02d.json' %(WEIGHTS_DIR, model_name, cut_block, train_layers)
-results_report_probas = '%s/result_probas_model_%s_%02d_%02d.csv' %(WEIGHTS_DIR, model_name, cut_block, train_layers)
+results_report = '%s/model_results_%s.json' %(RESULTS_DIR, model_name)
+results_report_probas = '%s/result_probas_model_%s.csv' %(RESULTS_DIR, model_name)
 
 print('*'*50)
 print('Testing started:')
 y_true, y_pred = test_model(device=device, model=model, test_loader=test_loader, PATH_RESULTS=results_report_probas)
 eval_preds(y_true, y_pred, results_report)
+
+# show model parent layers
+#for name, _ in model.named_children():
+#    print(name)
+
+if GRADCAM:
+    gradcam = GradCam(model=model, cam_layer_name=cam_layer, imagenet_norm=imagenet_norm)
+    computeGradCam(gradcam, test_loader, device, RESULTS_DIR, model_name)
+
+
 
 
 
